8345ac686a3d00f9e199a36ad520318dfd42a7ac
siillvergun <duejql03@naver.com>
2026-07-21 18:11:23 +0900

    chore: 개발 환경 세팅

---
+++ src/main/java/com/inuappcenter/team_2_project_server/Team2ProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.inuappcenter.team_2_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2ProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2ProjectServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inuappcenter/team_2_project_server/Team2ProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inuappcenter.team_2_project_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2ProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
