859044400b0ee5e5717bafb7a8922bd6d831ee18
siillvergun <duejql03@naver.com>
2026-08-11 00:28:15 +0900

    feat: 응답 예시 주석 추가

--- src/main/java/com/inuappcenter/team_2_project_server/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/inuappcenter/team_2_project_server/global/error/GlobalExceptionHandler.java
@@ -13,10 +13,16 @@ public class GlobalExceptionHandler {
 
     /**
      * 서비스(커스텀) 에러를 잡는 핸들러
+     * - 예시 응답
+     * {
+     * "data": null,
+     * "code": "INVALID_INPUT",
+     * "message": "잘못된 요청입니다."
+     * }
      */
     @ExceptionHandler(MyException.class)
     public ResponseEntity<ResponseDto<Integer>> handleMyException(MyException e) {
         log.error("예외 발생 msg:{}", e.getErrorCode().getMessage());
-        return ResponseEntity.status(e.getErrorCode().getStatus()).body(ResponseDto.of(null, e.getErrorCode().getMessage()));
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(ResponseDto.of(null, e.getErrorCode().getCode(), e.getErrorCode().getMessage()));
     }
 }
