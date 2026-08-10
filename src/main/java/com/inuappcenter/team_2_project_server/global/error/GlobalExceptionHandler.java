e415b76776e20c58e9096d53ada73d3e01df741d
siillvergun <duejql03@naver.com>
2026-08-11 00:22:52 +0900

    feat: 커스텀 에러 핸들러 추가

---
+++ src/main/java/com/inuappcenter/team_2_project_server/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.inuappcenter.team_2_project_server.global.error;
+
+import com.inuappcenter.team_2_project_server.global.dto.ResponseDto;
+import com.inuappcenter.team_2_project_server.global.error.ex.MyException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 서비스(커스텀) 에러를 잡는 핸들러
+     */
+    @ExceptionHandler(MyException.class)
+    public ResponseEntity<ResponseDto<Integer>> handleMyException(MyException e) {
+        log.error("예외 발생 msg:{}", e.getErrorCode().getMessage());
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(ResponseDto.of(null, e.getErrorCode().getMessage()));
+    }
+}
