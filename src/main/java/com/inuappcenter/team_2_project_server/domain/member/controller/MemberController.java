3add37614868502dc7af963cb72e12ba6d7650cf
siillvergun <duejql03@naver.com>
2026-08-11 15:01:15 +0900

    feat: member 로그인 컨트롤러 개발

---
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/member/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.inuappcenter.team_2_project_server.domain.member.controller;
+
+import com.inuappcenter.team_2_project_server.domain.member.dto.LoginRequestDto;
+import com.inuappcenter.team_2_project_server.domain.member.dto.LoginResponseDto;
+import com.inuappcenter.team_2_project_server.domain.member.service.MemberService;
+import com.inuappcenter.team_2_project_server.global.dto.ResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDto<LoginResponseDto>> login(
+            @Valid @RequestBody LoginRequestDto request
+    ) {
+
+        LoginResponseDto response = memberService.login(request);
+
+        return ResponseEntity.ok(
+                ResponseDto.of(response, "SUCCESS", "로그인 성공")
+        );
+    }
+}
