5fee4aa95d4512bd16f8e8e975bccd00f9c58885
siillvergun <duejql03@naver.com>
2026-08-11 15:00:16 +0900

    feat: 에러 코드 개발

--- src/main/java/com/inuappcenter/team_2_project_server/global/error/ex/ErrorCode.java
+++ src/main/java/com/inuappcenter/team_2_project_server/global/error/ex/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "INVALID_INPUT", "잘못된 요청입니다."),
-    ;
+    ORACLE_AUTH_UNAVAILABLE(HttpStatus.FORBIDDEN, "ORACLE_AUTH_UNAVAILABLE", "Oracle 연동을 실패했습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "INVALID_CREDENTIALS", "학번 또는 비밀번호가 올바르지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
