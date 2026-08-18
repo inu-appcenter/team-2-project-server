34209019016820d66ec43a68bbbac66a6cf33531
siillvergun <duejql03@naver.com>
2026-08-18 17:18:39 +0900

    feat: 로그인 응답, 요청 dto 개발

--- src/main/java/com/inuappcenter/team_2_project_server/domain/member/dto/LoginRequestDto.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/member/dto/LoginRequestDto.java
@@ -1,9 +1,15 @@
 package com.inuappcenter.team_2_project_server.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequestDto(
-        @NotBlank String studentNumber,
-        @NotBlank String password
+        @Schema(description = "학번", example = "202412345")
+        @NotBlank
+        String studentNumber,
+
+        @Schema(description = "비밀번호", example = "password1234")
+        @NotBlank
+        String password
 ) {
 }
