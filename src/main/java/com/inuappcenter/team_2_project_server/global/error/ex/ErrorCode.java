e7ab2d4d845572843811b4af3f3471596db6759a
siillvergun <duejql03@naver.com>
2026-08-23 13:23:18 +0900

    chore: 에러 코드 추가

--- src/main/java/com/inuappcenter/team_2_project_server/global/error/ex/ErrorCode.java
+++ src/main/java/com/inuappcenter/team_2_project_server/global/error/ex/ErrorCode.java
@@ -19,7 +19,8 @@ public enum ErrorCode {
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN_EXPIRED", "만료된 토큰입니다."),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TOKEN_INVALID", "유효하지 않은 토큰입니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "ACCESS_DENIED", "접근 권한이 없습니다."),
-    ;
+
+    RESEARCH_KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "RESEARCH_KEYWORD_NOT_FOUND", "검색 키워드가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
