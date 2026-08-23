7bc1410e0434257b1185dcca9a0969b87e6d0f61
siillvergun <duejql03@naver.com>
2026-08-23 17:19:20 +0900

    feat: 교수 레포지토리 개발

---
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/member/repository/ProfessorRepository.java
@@ -0,0 +1,11 @@
+package com.inuappcenter.team_2_project_server.domain.member.repository;
+
+import com.inuappcenter.team_2_project_server.domain.department.Department;
+import com.inuappcenter.team_2_project_server.domain.member.entity.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    Optional<Professor> findByDepartmentAndName(Department department, String name);
+}
