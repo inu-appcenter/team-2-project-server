09a3a4c117a29404c1617a13200caa547674c59e
siillvergun <duejql03@naver.com>
2026-08-24 17:49:24 +0900

    feat: 연구실 수정을 위한 요청 dto 개발

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryUpdateRequestDto.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryUpdateRequestDto.java
@@ -1,4 +1,11 @@
 package com.inuappcenter.team_2_project_server.domain.laboratory.dto.request;
 
-public class LaboratoryUpdateRequestDto {
+public record LaboratoryUpdateRequestDto(
+        String labName,
+        String location,
+        Long capacity,
+        String introduction,
+        String labUrl,
+        String researchFieldRaw
+) {
 }
