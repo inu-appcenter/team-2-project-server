eaa419501c0e9d5b6c974ae056fadafe26db50dc
siillvergun <duejql03@naver.com>
2026-08-25 15:13:02 +0900

    feat: DTO 검증 및 인원수 관련 dto를 필드에 추가

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryCreateRequestDto.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryCreateRequestDto.java
@@ -2,18 +2,21 @@ package com.inuappcenter.team_2_project_server.domain.laboratory.dto.request;
 
 import com.inuappcenter.team_2_project_server.domain.department.College;
 import com.inuappcenter.team_2_project_server.domain.department.Department;
+import com.inuappcenter.team_2_project_server.domain.laboratory.dto.LaboratoryCapacityDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record LaboratoryCreateRequestDto(
-        College college,
-        Department department,
-        String labName,
-        String location,
-        Long capacity,
+        @NotNull College college,
+        @NotNull Department department,
+        @NotBlank String labName,
+        @NotBlank String location,
+        @NotNull LaboratoryCapacityDto capacity,
         String introduction,
-        Long professorId,
-        String labUrl,
+        @NotNull Long professorId,
+        @NotBlank String labUrl,
         List<String> researchAreas
 ) {
 }
