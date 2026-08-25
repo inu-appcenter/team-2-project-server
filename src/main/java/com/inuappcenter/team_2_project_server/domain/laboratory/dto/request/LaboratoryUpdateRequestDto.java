14d9a712b33e57240d01024071fc14bad0eef9db
siillvergun <duejql03@naver.com>
2026-08-25 15:13:54 +0900

    feat: 인원수 관련 필드 추가

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryUpdateRequestDto.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LaboratoryUpdateRequestDto.java
@@ -1,11 +1,13 @@
 package com.inuappcenter.team_2_project_server.domain.laboratory.dto.request;
 
+import com.inuappcenter.team_2_project_server.domain.laboratory.dto.LaboratoryCapacityDto;
+
 import java.util.List;
 
 public record LaboratoryUpdateRequestDto(
         String labName,
         String location,
-        Long capacity,
+        LaboratoryCapacityDto capacity,
         String introduction,
         String labUrl,
         List<String> researchAreas
