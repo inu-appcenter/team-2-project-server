c2d65a8da779869b96b5a5cb86b57fbe0dcc52b7
siillvergun <duejql03@naver.com>
2026-08-29 14:43:57 +0900

    feat: 파싱한 논문 row 저장 로직 개발

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/repository/LaboratoryRepository.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/repository/LaboratoryRepository.java
@@ -5,12 +5,14 @@ import com.inuappcenter.team_2_project_server.domain.member.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LaboratoryRepository extends JpaRepository<Laboratory, Long> {
+    Optional<Laboratory> findByLabNameAndProfessor_Name(String labName, String professorName);
+
     boolean existsByLabNameAndProfessor(String labName, Professor professor);
 
     boolean existsByLabNameAndProfessorId(String labName, Long professorId);
-
 
     List<Laboratory> findByLabNameContainingIgnoreCaseOrProfessor_NameContainingIgnoreCase(
             String labNameKeyword,
