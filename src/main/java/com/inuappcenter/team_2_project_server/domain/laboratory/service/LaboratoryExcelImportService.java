c6f935544b9aa7a167512ef4a0fcee2cf013b37c
siillvergun <duejql03@naver.com>
2026-08-29 14:43:52 +0900

    feat: 파싱한 논문 row 저장 로직 개발

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/service/LaboratoryExcelImportService.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/service/LaboratoryExcelImportService.java
@@ -3,11 +3,14 @@ package com.inuappcenter.team_2_project_server.domain.laboratory.service;
 import com.inuappcenter.team_2_project_server.domain.department.Department;
 import com.inuappcenter.team_2_project_server.domain.laboratory.dto.parse.LaboratoryExcelRow;
 import com.inuappcenter.team_2_project_server.domain.laboratory.dto.parse.ProfessorExcelRow;
+import com.inuappcenter.team_2_project_server.domain.laboratory.dto.parse.PublicationExcelRow;
 import com.inuappcenter.team_2_project_server.domain.laboratory.entity.Laboratory;
 import com.inuappcenter.team_2_project_server.domain.laboratory.entity.LaboratoryResearchArea;
+import com.inuappcenter.team_2_project_server.domain.laboratory.entity.Publication;
 import com.inuappcenter.team_2_project_server.domain.laboratory.entity.ResearchArea;
 import com.inuappcenter.team_2_project_server.domain.laboratory.repository.LaboratoryRepository;
 import com.inuappcenter.team_2_project_server.domain.laboratory.repository.LaboratoryResearchKeywordRepository;
+import com.inuappcenter.team_2_project_server.domain.laboratory.repository.PublicationRepository;
 import com.inuappcenter.team_2_project_server.domain.laboratory.repository.ResearchKeywordRepository;
 import com.inuappcenter.team_2_project_server.domain.member.entity.Professor;
 import com.inuappcenter.team_2_project_server.domain.member.repository.ProfessorRepository;
@@ -36,6 +39,7 @@ public class LaboratoryExcelImportService {
     private final LaboratoryRepository laboratoryRepository;
     private final ResearchKeywordRepository researchKeywordRepository;
     private final LaboratoryResearchKeywordRepository laboratoryResearchKeywordRepository;
+    private final PublicationRepository publicationRepository;
 
     public void importExcel(MultipartFile file) {
         validateExcelFile(file);
@@ -43,12 +47,17 @@ public class LaboratoryExcelImportService {
         // 파싱 클래스에서 파싱해 ProfessorExcelRow과 LaboratoryExcelRow로 만든 행들을 rows에 저장
         List<ProfessorExcelRow> professorRows = excelLabParser.parseProfessors(file);
         List<LaboratoryExcelRow> laboratoryRows = excelLabParser.parseLaboratories(file);
+        List<PublicationExcelRow> publicationExcelRows = excelLabParser.parsePublications(file);
 
         saveProfessors(professorRows);
         saveLaboratories(laboratoryRows, professorRows);
+        savePublications(publicationExcelRows);
     }
 
 
+    /**
+     * 연구실 파싱 후 저장
+     */
     private void saveLaboratories(
             List<LaboratoryExcelRow> laboratoryRows,
             List<ProfessorExcelRow> professorRows
@@ -127,6 +136,9 @@ public class LaboratoryExcelImportService {
     }
 
 
+    /**
+     * 연구 분야 키워드 저장
+     */
     private void saveResearchKeywords(Laboratory laboratory, String researchFieldRaw) {
         List<String> keywordNames = splitResearchKeywords(researchFieldRaw);
 
@@ -172,5 +184,41 @@ public class LaboratoryExcelImportService {
 
     private String professorKey(Department department, String professorName, String professorEmail) {
         return department.name() + "|" + professorName + "|" + professorEmail;
+    }
+
+    /**
+     * 논문 저장 메서드
+     */
+    private void savePublications(List<PublicationExcelRow> rows) {
+        for (PublicationExcelRow row : rows) {
+            Laboratory laboratory = laboratoryRepository.findByLabNameAndProfessor_Name(row.laboratoryName(), row.professorName())
+                    .orElseThrow(() -> new MyException(ErrorCode.LABORATORY_NOT_FOUND));
+
+            Professor professor = laboratory.getProfessor();
+
+            if (publicationRepository.existsByLaboratoryAndTitleAndYearAndSource(
+                    laboratory,
+                    row.title(),
+                    row.year(),
+                    row.source()
+            )) {
+                continue;
+            }
+
+            Publication publication = Publication.create(
+                    laboratory,
+                    professor,
+                    row.title(),
+                    row.researchersRaw(),
+                    row.source(),
+                    row.year(),
+                    row.type(),
+                    row.status(),
+                    row.doi(),
+                    row.sourceURL()
+            );
+
+            publicationRepository.save(publication);
+        }
     }
 }
