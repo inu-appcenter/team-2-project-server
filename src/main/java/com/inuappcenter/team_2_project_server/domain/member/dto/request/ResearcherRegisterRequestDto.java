7865c2d354334f3420cd47f972c50885042dae5d
siillvergun <duejql03@naver.com>
2026-09-02 14:33:29 +0900

    feat: 연구자 등록 요청 DTO 개발

---
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/member/dto/request/ResearcherRegisterRequestDto.java
@@ -0,0 +1,12 @@
+package com.inuappcenter.team_2_project_server.domain.member.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * 연구자 등록 요청 DTO
+ */
+public record ResearcherRegisterRequestDto(
+        @NotNull Long memberId,
+        @NotNull Long laboratoryId
+) {
+}
