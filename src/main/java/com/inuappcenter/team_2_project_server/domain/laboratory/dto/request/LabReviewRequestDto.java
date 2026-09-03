aabb1fad6400ac5ddf5e3408cff0fc7824df5ce0
siillvergun <duejql03@naver.com>
2026-09-03 15:16:28 +0900

    feat: 연구실 리뷰 요청 dto에 크기 제한 추가

--- src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LabReviewRequestDto.java
+++ src/main/java/com/inuappcenter/team_2_project_server/domain/laboratory/dto/request/LabReviewRequestDto.java
@@ -2,12 +2,13 @@ package com.inuappcenter.team_2_project_server.domain.laboratory.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
 public record LabReviewRequestDto(
-        @NotBlank String coreTime,
-        @NotBlank String weeklyMeeting,
-        @NotEmpty Set<@NotBlank String> doings
+        @NotBlank @Size(max = 30) String coreTime,
+        @NotBlank @Size(max = 30) String weeklyMeeting,
+        @NotEmpty @Size(max = 20) Set<@NotBlank String> doings
 ) {
 }
